9e65ccacd8a0e19f597a2af7c597e6af2566ec5f
Serge <sergesoroka@gmail.com>
2023-11-30 12:31:15 +0200

    LineChart Test: styles

--- components/LineChart/LineChart.tsx
+++ components/LineChart/LineChart.tsx
@@ -23,7 +23,7 @@ const data = [
 export default function LineChartComp() {
   return (
     <LineChart
-      width={600}
+      width={1000}
       height={300}
       data={data}
       margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
