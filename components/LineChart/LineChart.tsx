2569208ecf07f7d04a735137cb0a18469287de5c
Serge <sergesoroka@gmail.com>
2023-11-30 20:26:03 +0200

    Home page: Legend For LineChart

--- components/LineChart/LineChart.tsx
+++ components/LineChart/LineChart.tsx
@@ -13,13 +13,26 @@ import {
 } from "recharts";
 
 import { lineChartData } from "./data";
+import { lineChartAllData } from "./all_data";
 
 const CustomTooltip = ({ active, payload, label }) => {
+  console.log(payload);
+
   if (active && payload && payload.length) {
     return (
       <div className="custom-tooltip bg-white p-4">
-        <p className="label">{`Round: ${label}`}</p>
-        <p className="label">{`Total: ${payload[0].value}`}</p>
+        <p className="label text-[12px] text-red-700 font-bold mb-1">{`Round: #${label}`}</p>
+        <p className="label text-[12px] mb-2">{`${payload[0].payload.date_round}`}</p>
+        <hr />
+
+        <p className="label text-[12px] mt-2">{`i1: ${payload[0].payload.i1}`}</p>
+        <p className="label text-[12px]">{`i2: ${payload[0].payload.i2}`}</p>
+        <p className="label text-[12px]">{`i3: ${payload[0].payload.i3}`}</p>
+        <p className="label text-[12px]">{`i4: ${payload[0].payload.i4}`}</p>
+        <p className="label text-[12px]">{`i5: ${payload[0].payload.i5}`}</p>
+        <p className="label text-[12px]">{`i6: ${payload[0].payload.i6}`}</p>
+        <hr />
+        <p className="label text-[12px] mt-2 text-green-600">{`Total: ${payload[0].payload.total}`}</p>
         {/* <p className="desc">Anything you want can be displayed here.</p> */}
       </div>
     );
@@ -33,7 +46,7 @@ export default function LineChartComp() {
     <LineChart
       width={1000}
       height={300}
-      data={lineChartData}
+      data={lineChartAllData}
       margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
       style={{ width: "100%", height: "100%" }}
     >
