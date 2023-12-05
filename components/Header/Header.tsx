4e44d0ae62ff73078f90955045125e049bb268be
Serge <sergesoroka@gmail.com>
2023-12-05 12:30:45 +0200

    Logo lang

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,16 +2,36 @@ import LangSwitcher from "./LangSwitcher/LangSwitcher";
 import styles from "./Header.module.css";
 import Navbar from "./Navbar/Navbar";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
+  const { locale } = router;
   return (
     <header className={styles.header}>
       <Link href="/" passHref>
-        <div className="text-xl font-medium leading-5 text-gray-600">
-          Iндекс
-          <br />
-          реформ
-        </div>
+        {locale == "ua" && (
+          <div className="text-xl font-medium leading-5 text-gray-600">
+            Iндекс
+            <br />
+            реформ
+          </div>
+        )}
+
+        {locale == "ru" && (
+          <div className="text-xl font-medium leading-5 text-gray-600">
+            Индекс
+            <br />
+            реформ
+          </div>
+        )}
+        {locale == "en" && (
+          <div className="text-xl font-medium leading-5 text-gray-600">
+            Reforms
+            <br />
+            index
+          </div>
+        )}
       </Link>
       <Navbar />
       <LangSwitcher />
