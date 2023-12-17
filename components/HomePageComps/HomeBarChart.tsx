5363d1aa37cbf3e0cfeb295ba97e035658e941e0
Serge <sergesoroka@gmail.com>
2023-12-17 11:32:05 +0200

    Area Chart: fullwidth

--- components/HomePageComps/HomeBarChart.tsx
+++ components/HomePageComps/HomeBarChart.tsx
@@ -6,7 +6,7 @@ import AreaChartComp from "components/LineChart/AreaChart";
 
 function HomeBarChart() {
   return (
-    <div className="flex flex-col gap-16">
+    <div className="flex flex-col gap-16 w-full">
       <AreaChartComp />
       {/* <LineChartComp /> */}
     </div>
