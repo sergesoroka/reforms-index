16a7d5aa4b5944a2b7c6458133d9570aa6ded6b0
Serge <sergesoroka@gmail.com>
2024-01-02 13:10:59 +0200

    yAxis issue, minmax

--- components/LineChart/AreaChart.tsx
+++ components/LineChart/AreaChart.tsx
@@ -45,9 +45,12 @@ export default function AreaChartComp() {
     locale == "en" ? "Round" : locale == "ru" ? "Раунд" : "Раунд";
 
   const uniqYears = [];
+  const marksValue = [];
+
   const formattedData =
     data &&
     data.data.map((item) => {
+      marksValue.push(item.mark);
       if (
         !uniqYears.includes(item.date_end.toString().substring(6)) &&
         item.date_end.toString().substring(6) !== ""
@@ -63,6 +66,9 @@ export default function AreaChartComp() {
         year: item.date_end.toString().substring(6),
       };
     });
+
+  console.log(Math.max(...marksValue));
+  console.log(Math.min(...marksValue));
 
   const formattedPeriod = (startDate, endDate) => {
     let startDay = startDate && startDate.substring(0, 2);
