79843dcc6cfeba3a46c789c025b5fcf74c6f19ab
Андрей Зиновьев <andriy.zionoviev@gmail.com>
2024-01-10 22:22:54 +0200

    add dynamic menu render

--- components/Header/Navbar/Navbar.tsx
+++ components/Header/Navbar/Navbar.tsx
@@ -16,118 +16,53 @@ const Navbar = ({ data }) => {
     },
   };
 
-  return (
-    <>
-      <ul className={styles.navbar}>
-        {data &&
-          data.data.header_menu.map((item, i) => {
-            if (item.children.length == 0) {
-              return (
-                <li key={item.id}>
-                  <Link
-                    href={item.value ? item.value : ""}
-                    passHref
-                    className={
-                      pathname === item.value
-                        ? styles.navActiveItem
-                        : styles.navItem
-                    }
-                  >
-                    {item.name}
-                  </Link>
-                  {pathname === item.value && (
-                    <motion.div
-                      variants={variants}
-                      initial="hidden"
-                      animate="visible"
-                      className="h-1 w-full bg-red-600"
-                    />
-                  )}
-                </li>
-              );
+  return (<>
+        <ul className={styles.navbar}>
+          {data && data.data.header_menu.map((item, i) => {
+            if (item.children.length == 0 && item.type != 'text') {
+              return (<li key={item.id}>
+                    <Link
+                        href={item.value ? item.value : ""}
+                        passHref
+                        className={pathname === item.value ? styles.navActiveItem : styles.navItem}
+                        target={item.target}
+                    >
+                      {item.name}
+                    </Link>
+                    {pathname === item.value && (<motion.div
+                            variants={variants}
+                            initial="hidden"
+                            animate="visible"
+                            className="h-1 w-full bg-red-600"
+                        />)}
+                  </li>);
+            } else if (item.children.length > 0) {
+              return (<li>
+                    <div className={styles.dropdown}>
+                      <button className={styles.dropbtn}>
+                        {item.name}
+                      </button>
+                      <div className={styles.dropdownContent}>
+                        {item.children.map((sub_item, i) => {
+                          return (<Link
+                                  href={sub_item.value}
+                                  passHref
+                                  target={sub_item.target}
+                              >
+                                {sub_item.name}
+                              </Link>);
+
+                        })}
+                      </div>
+                    </div>
+                  </li>
+              )
             }
-            // if (item.children.length > 0) {
-            //   item.children.map((link) => console.log(link.name));
-            // }
           })}
 
-        <li>
-          <div className={styles.dropdown}>
-            {/* <button className={styles.dropbtn}>
-              {data && data.data.header_menu[3].name}
-            </button>
-            <div className={styles.dropdownContent}>
-              {data &&
-                data.data.header_menu.map((item) => {
-                  if (item.children.length > 0) {
-                    item.children.map((link) => console.log(link.name));
-                    return (
-                      <Link
-                        key={item.id}
-                        href={
-                          data ? data.data.header_menu[3].children[0].value : ""
-                        }
-                        passHref
-                        target="_blank"
-                      >
-                        {item.children[0].name}
-                      </Link>
-                    );
-                  }
-                })}
-            </div> */}
-            <button className={styles.dropbtn}>
-              {data && data.data.header_menu[3].name}
-            </button>
-            <div className={styles.dropdownContent}>
-              <Link
-                href={data ? data.data.header_menu[3].children[0].value : ""}
-                passHref
-                target="_blank"
-              >
-                {data && data.data.header_menu[3].children[0].name}
-              </Link>
-              <Link
-                href={data ? data.data.header_menu[3].children[1].value : ""}
-                passHref
-                target="_blank"
-              >
-                {data && data.data.header_menu[3].children[1].name}
-              </Link>
-            </div>
-          </div>
 
-          {pathname === "/data" && (
-            <motion.div
-              variants={variants}
-              initial="hidden"
-              animate="visible"
-              className="h-1 w-full bg-red-600"
-            />
-          )}
-        </li>
-        {/* <li>
-          <Link
-            href="/laws"
-            passHref
-            className={
-              pathname === "/laws" ? styles.navActiveItem : styles.navItem
-            }
-          >
-            {lawsLabel}
-          </Link>
-          {pathname === "/laws" && (
-            <motion.div
-              variants={variants}
-              initial="hidden"
-              animate="visible"
-              className="h-1 w-full bg-red-600"
-            />
-          )}
-        </li> */}
-      </ul>
-    </>
-  );
+        </ul>
+      </>);
 };
 
 export default Navbar;
