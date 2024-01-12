990611609b39eab6186f44ce9389f3a45ae03514
Serge <sergesoroka@gmail.com>
2024-01-12 19:19:55 +0200

    Table styles

--- src/pages/method.tsx
+++ src/pages/method.tsx
@@ -7,8 +7,8 @@ export default function Method({ data }) {
     data.data.map((page, i) => {
       if (page.id == 2) {
         return (
-          <div key={i}>
-              <MetaData data={page}/>
+          <div key={i} className="pageMethod">
+            <MetaData data={page} />
             <div className="mb-4">
               <h1 className="mb-8">{page.title}</h1>
               <p dangerouslySetInnerHTML={{ __html: page.content }} />
