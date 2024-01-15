d4a7054b80d228f2ebba2e3501d2b89eda9d91be
Serge <sergesoroka@gmail.com>
2024-01-15 12:59:55 +0200

    Favicon and Links Styles

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const Header = ({ data }) => {
   return (
     <header className={styles.header}>
-      <Link href="/" passHref className="w-[150px]">
+      <Link href="/" passHref className="w-[150px] no-underline">
         <h1 className="[word-spacing:9999px] text-xl font-medium leading-5 mt-2 text-gray-600">
           {data && data.data.logo}
         </h1>
