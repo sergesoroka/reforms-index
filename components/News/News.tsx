8325159cdf84dfb84ef70deed7864def5e292986
Serge <sergesoroka@gmail.com>
2024-02-22 18:54:46 +0200

    News Page

---
+++ components/News/News.tsx
@@ -0,0 +1,47 @@
+import Divider from "components/Divider/Divider";
+import SourceIcon from "components/IconsComponents/SourceIcon";
+
+// 2014-02-21
+
+export default function News({ data }) {
+  const formattedDate = (date) => {
+    let day = date && date.substring(8, 10);
+    let month = date && date.substring(5, 7);
+    let year = date && date.substring(0, 4);
+    return (
+      <span>
+        {day}.{month}.{year}
+      </span>
+    );
+  };
+
+  const contentRender = data?.data.content.map((item, i) => {
+    return (
+      <div key={i}>
+        <div className="flex justify-between items-center text-xs px-2">
+          <p className="font-semibold text-xs">{formattedDate(item.date)}</p>
+          <div className="flex items-center gap-2 text-red-700 uppercase font-bold">
+            <span className="text-red-700">{item.source}</span>
+            <SourceIcon />
+          </div>
+        </div>
+        <div
+          className="text-[16px] px-2"
+          dangerouslySetInnerHTML={{ __html: item.body }}
+        />
+        <Divider heading="" gray={true} openable={false} single={false} />
+      </div>
+    );
+  });
+  return (
+    <div>
+      <div className="w-[76px] h-[30px] bg-[#00B63E] text-white font-bold uppercase text-xs mx-2 px-3 py-2 mb-6">
+        {data?.data.tag}
+      </div>
+      <h1 className="px-2">{data?.data.title}</h1>
+      <Divider single={true} />
+
+      {contentRender}
+    </div>
+  );
+}
---
+++ src/pages/news/[id].tsx
@@ -0,0 +1,54 @@
+import Head from "next/head";
+import Header from "components/Header/Header";
+import Footer from "components/Footer/Footer";
+import News from "components/News/News";
+import styles from "@/styles/Home.module.css";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import { fetcher } from "lib/fetcher";
+
+export default function NewsPage({ baseURL, setting }) {
+  const router = useRouter();
+  const { locale, asPath } = router;
+
+  const { data, isLoading, isValidating } = useSWR(
+    `${baseURL}/api${asPath}?lang=${locale}`,
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    }
+  );
+
+  console.log(data);
+
+  return (
+    <>
+      <Header data={setting} />
+      <main className={styles.mainAboutPage}>
+        <News data={data} />
+      </main>
+      <Footer data={setting} />
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const res = await fetch(
+    `${process.env.baseURL}/api/pages?lang=${context.locale}`
+  );
+  const resSetting = await fetch(
+    `${process.env.baseURL}/api/settings?lang=${context.locale}`
+  );
+  const metadata = await res.json();
+  const setting = await resSetting.json();
+
+  return {
+    props: {
+      metadata,
+      baseURL: process.env.baseURL,
+      setting,
+    },
+  };
+}
