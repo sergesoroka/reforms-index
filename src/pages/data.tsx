6425cf47d44d2799d255351b99d3e48359875b5c
Serge <sergesoroka@gmail.com>
2024-03-13 09:51:41 +0200

    Table page, data fetching, removed old table

--- src/pages/data.tsx
+++ src/pages/data.tsx
@@ -5,7 +5,7 @@ import styles from "@/styles/Home.module.css";
 export default function Home() {
   return (
     <>
-      <Head>
+      {/* <Head>
         <title>The Interactive Table</title>
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -15,7 +15,7 @@ export default function Home() {
       <main className={styles.main}>
         <h2 className="text-xl mb-8">Table Data</h2>
         <Table />
-      </main>
+      </main> */}
     </>
   );
 }
