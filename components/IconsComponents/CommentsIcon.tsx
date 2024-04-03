2906eb0c4bbe7f414ab022e32b9159026683d4a6
Serge <sergesoroka@gmail.com>
2024-04-03 17:14:59 +0300

    Added Links in Table, comments icon

---
+++ components/IconsComponents/CommentsIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function CommentsIcon() {
+  return (
+    <svg
+      width="24"
+      height="21"
+      viewBox="0 0 24 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 2C1 1.44771 1.44771 1 2 1H22C22.5523 1 23 1.44772 23 2V12.6854C23 13.2377 22.5523 13.6854 22 13.6854H7.31077C6.61214 13.6854 5.93542 13.9293 5.39734 14.3749L2.63781 16.6601C1.98592 17.1999 1 16.7363 1 15.8899V2Z"
+        stroke="#636363"
+        stroke-width="2"
+      />
+      <ellipse cx="7.5" cy="7.5" rx="1.5" ry="1.5" fill="#636363" />
+      <ellipse cx="12" cy="7.5" rx="1.5" ry="1.5" fill="#636363" />
+      <ellipse cx="16.5" cy="7.5" rx="1.5" ry="1.5" fill="#636363" />
+    </svg>
+  );
+}
--- components/Table/TableBody.tsx
+++ components/Table/TableBody.tsx
@@ -2,6 +2,8 @@ import styles from "./Table.module.css";
 import columns from "./../../data/tableColumns.json";
 
 import Flag from "components/IconsComponents/Flag";
+import CommentsIcon from "components/IconsComponents/CommentsIcon";
+import Link from "next/link";
 
 const TableBody = ({ tableData }) => {
   return (
@@ -12,10 +14,58 @@ const TableBody = ({ tableData }) => {
             {columns.map(({ accessor }) => {
               const tData = row[accessor] ? row[accessor] : "——";
 
+              if (accessor == "npa_links[0].doc_type") {
+                return (
+                  <td key={accessor} className={styles.tableBodyCell}>
+                    <Link
+                      className="underline underline-offset-2 capitalize whitespace-nowrap hover:text-red-600"
+                      href={
+                        row.npa_links[0]?.link ? row.npa_links[0]?.link : ""
+                      }
+                    >
+                      {row.npa_links[0]?.doc_type}
+                    </Link>
+                    <p>
+                      <Link
+                        className="underline underline-offset-2 capitalize whitespace-nowrap hover:text-red-600"
+                        href={
+                          row.npa_links[1]?.link ? row.npa_links[1]?.link : ""
+                        }
+                      >
+                        {row.npa_links[1]?.doc_type}
+                      </Link>
+                    </p>
+                  </td>
+                );
+              }
+
               if (accessor == "npa_links[0].initiator") {
                 return (
                   <td key={accessor} className={styles.tableBodyCell}>
-                    <p>{row.npa_links[0]?.initiator}</p>
+                    {row.npa_links[0]?.initiator}
+                  </td>
+                );
+              }
+
+              if (accessor == "grade_total") {
+                return (
+                  <td key={accessor} className={styles.tableBodyCell}>
+                    <span className="text-lg">{row.grade_total}</span>
+                  </td>
+                );
+              }
+
+              if (accessor == "title") {
+                return (
+                  <td key={accessor} className={styles.tableBodyCell}>
+                    {row.title}
+                    <p className="flex gap-4 text-[11px] font-medium pt-2 text-gray-500">
+                      <span>Новина</span>
+                      <span className="flex gap-1">
+                        <CommentsIcon />
+                        <span className="text-red-600">0</span>
+                      </span>
+                    </p>
                   </td>
                 );
               }
@@ -23,8 +73,8 @@ const TableBody = ({ tableData }) => {
               if (accessor == "date") {
                 return (
                   <td key={accessor} className={styles.tableBodyCell}>
-                    <p>{row.date}</p>
-                    <div>{row.flag == 0 ? <Flag /> : null}</div>
+                    <p className="whitespace-nowrap">{row.date}</p>
+                    <div>{row.flag == 1 ? <Flag /> : null}</div>
                   </td>
                 );
               }
