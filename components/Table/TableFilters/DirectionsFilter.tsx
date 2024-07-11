4057200ea6b44fb1a014f7c88cebe3cee979b42e
Андрей Зиновьев <andriy.zionoviev@gmail.com>
2024-07-11 17:45:21 +0300

    add directions filter

--- components/Table/TableFilters/DirectionsFilter.tsx
+++ components/Table/TableFilters/DirectionsFilter.tsx
@@ -4,6 +4,7 @@ import CheckBox from "./DirectionCheckBox";
 
 import useSWR from "swr";
 import { fetcher } from "lib/fetcher";
+import NestedDirectionCheckBox from "./DirectionFilter/NestedDirectionCheckBox";
 
 export default function DirectionsFilter({
   baseURL,
@@ -21,73 +22,22 @@ export default function DirectionsFilter({
     }
   );
 
-  console.log(directions);
-  useEffect(() => {
-    data &&
-      data.data.map((item) => {
-        if (!directions.includes(item)) {
-          setDirections(directions.filter((d) => d !== item));
-        }
-      });
-  }, [selected]);
-
   return (
-    <div className="flex flex-col gap-2 justify-start items-start">
-      {data &&
-        data.data.map((item, i) => {
-          return (
-            <div key={item.id} className="flex justify-start gap-6">
-              <div
-                onClick={() => {
-                  if (!directions.includes(item)) {
-                    setDirections([...directions, item]);
-                  } else {
-                    setDirections(directions.filter((d) => d !== item.name));
-                  }
-                }}
-              >
-                <CheckBox
-                  label={item.name}
-                  item={item}
-                  docTypes={directions}
-                  setDocTypes={setDirections}
-                  setSelected={setSelected}
-                  selected={selected}
-                />
-              </div>
-
-              {directions.includes(item)
-                ? item.sub_directions.map((sub, y) => {
-                    return (
-                      <div
-                        onClick={() => {
-                          if (directions.includes(item)) {
-                            setDirections([...directions, sub]);
-                          } else {
-                            setDirections(
-                              directions.filter((dir) => dir !== sub)
-                            );
-                          }
-                        }}
-                        key={sub.id}
-                        className="flex items-center justify-start gap-2 cursor-pointer"
-                      >
-                        <p
-                          className={`${
-                            directions.includes(sub)
-                              ? "text-red-600"
-                              : "text-gray-400"
-                          }`}
-                        >
-                          {sub.name}
-                        </p>
-                      </div>
-                    );
-                  })
-                : null}
-            </div>
-          );
-        })}
-    </div>
+      <div>
+        <div className="space-y-2">
+          {data &&
+              data.data.map((item, i) => {
+                return(
+                    <NestedDirectionCheckBox
+                        key={item['id']}
+                        label={item['name']}
+                        dataSet={item['sub_directions']}
+                        values={directions}
+                        setValues={setDirections}
+                    />
+                )
+              })}
+        </div>
+      </div>
   );
 }
